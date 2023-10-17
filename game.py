a6a0d26b94b10f40f7c57924ae0c38bca79ea0ef
TerrorFusion <99619094+TerrorFusion@users.noreply.github.com>
2023-10-17 18:24:33 +0100

    added menu logic

--- game.py
+++ game.py
@@ -13,10 +13,28 @@ def mainMenu():
     print("|_____||_____||__|__|\____||__|\_| \____||__|__|\____||__|\_| ")
     print("                                                             ")
     print("1. Play")
-    print("2. Instructions")
+    print("2. Rules")
     print("3. Quit")
     print("4. Credits")
     print(" ")
     selection = input("What would you like to do? [1-4]: ")
-    return
 
+    if selection == "1":
+        play()
+         
+    elif selection == "2":
+        rules()
+
+    elif selection == "3":
+        exit(1)
+
+    elif selection == "4":
+        credits()
+
+    else:
+        print("That was and ivlaid input, please try again")
+        time.sleep(15)
+        mainMenu()
+
+
+    return
