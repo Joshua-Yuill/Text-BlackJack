0e3b939ef64e33b61fe7d6c20cd7f5517128b411
Joshua-Yuill <78020975+Joshua-Yuill@users.noreply.github.com>
2023-10-17 17:53:29 +0100

    Initial commit

---
+++ game.py
@@ -0,0 +1,14 @@
+#Libraries
+import os
+
+#Functions
+def mainMenu():
+    os.system('cls')
+    print(" ____   _       ____    __  __  _   ____   ____    __  __  _ ")
+    print("|    \ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]")
+    print("|  o  )| |    |  o  | /  / |  ' /  |__  ||  o  | /  / |  ' / ")
+    print("|    \ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]")
+    print("|    \ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]")
+    print("|    \ | |     /    |  /  ]|  |/ ] |    | /    |  /  ]|  |/ ]")
+    print
+    return
