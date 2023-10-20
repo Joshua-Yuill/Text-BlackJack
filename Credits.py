afd3ac4a624cc8714fda29a5ab1fe5f86fbd018d
TerrorFusion <99619094+TerrorFusion@users.noreply.github.com>
2023-10-20 20:22:01 +0100

    added fonts and colours to text, stopped resizing of window

--- Credits.py
+++ Credits.py
@@ -5,7 +5,7 @@ window = tk.Tk()
 window.title("Thank You Credits")
 
 # Set the window size and position it in the center of the screen
-window.geometry("400x300")
+window.geometry("550x225")
 window.eval('tk::PlaceWindow . center')
 
 # Function to close the window
@@ -13,15 +13,15 @@ def close_window():
     window.destroy()
 
 # Create a frame to hold the content and set its background color
-content_frame = tk.Frame(window, bg="lightgray")
+content_frame = tk.Frame(window, bg="lightgrey")
 content_frame.pack(fill=tk.BOTH, expand=True)
 
 # Create a "Thank You" label with a larger font
-thank_you_label = tk.Label(content_frame, text="Thank You For Playing Our Game ", font=("Helvetica", 24))
+thank_you_label = tk.Label(content_frame, text="Thank You For Playing Our Game ", font=("Verdana", 24), fg="blue", bg="lightgrey")
 thank_you_label.pack(pady=20)
 
 # Create a label for contributors
-contributors_label = tk.Label(content_frame, text="Special thanks to our contributors:")
+contributors_label = tk.Label(content_frame, text="Special thanks to our contributors:", font=("Verdana", 16), fg="red", bg="lightgrey")
 contributors_label.pack()
 
 # Create a list of contributors
@@ -30,8 +30,13 @@ contributors = [
     "TerrorFusion",
 ]
 
+#contributor font and size and colour
+contributor_font = ("Verdana", 12)
+contributor_color = "#32a87d"
+contributor_bg_color = "lightgrey"
+
 for contributor in contributors:
-    contributor_label = tk.Label(content_frame, text=contributor)
+    contributor_label = tk.Label(content_frame, text=contributor, font=contributor_font, fg=contributor_color, bg=contributor_bg_color)
     contributor_label.pack()
 
 # Create a "Close" button
